649d77e9e8a5a375b11ead46d68af9c544763569
Ghastrum <dennis.malmin@tuta.com>
2026-06-17 22:57:56 +0200

    added changelog and small name change for nixpkgs unstable compliance

--- nix/package.nix
+++ nix/package.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "areofyl";
     repo = "fetch";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-9ixx7XJcY4ktcN/lUfjvFljvHIEO2ktOebeGgL0ulHg=";
   };
 
@@ -29,6 +29,7 @@ stdenv.mkDerivation (finalAttrs: {
   meta = {
     description = "Animated 3D fetch tool that renders your distro logo as a spinning bas-relief";
     homepage = "https://github.com/areofyl/fetch";
+    changelog = "https://github.com/areofyl/fetch/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.isc;
     maintainers = with lib.maintainers; [ ghastrum ];
     mainProgram = "fetch";
